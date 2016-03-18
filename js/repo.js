9aa3502de736d9e7c8dbd830eb1f72d4767aa679
Lauryn <lryndavis@gmail.com>
2016-03-18 11:26:47 -0700

    add additional key value pairs to fetch from repo json

--- js/repo.js
+++ js/repo.js
@@ -4,13 +4,20 @@ exports.getRepo = function(userName){
   $.get('https://api.github.com/users/'+userName+'/repos?access_token=' + apiKey).then(function(response){
     $.each(response, function(i, repo) {
       console.log(response);
-      // numofRepos = response.length;
+      numofRepos = response.length;
       name = this.name;
-      description = this.description;
-      $(".repositories").append('<li>' + name + '</li>');
-
+      linkTo = this.url;
+      stars = this.stargazers_count;
+      if (this.description !== "") {
+          repoDescription = this.description;
+        } else {
+          repoDescription = "Description not Provided.";
+        }
+      $(".repositories").append('<li>' + name + '</li>' +
+                                '<li>' + repoDescription + '</li>');
+      $(".amount").text(numofRepos);
     });
   }).fail(function(error){
-    console.log(error.responseJSON.message);
+    $(".repositories").text(error.responseJSON.message);
   });
 };
